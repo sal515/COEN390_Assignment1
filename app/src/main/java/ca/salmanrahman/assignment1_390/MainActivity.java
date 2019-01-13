507a1dd1d5cff15ed45f21e02cf99f41db8bf4e0
Salman <rahman_5@live.ca>
2019-01-12 23:27:45 -0500

    Setting up the mainActivity page with button and textview Also, playing with the theme color scheme

--- app/src/main/java/ca/salmanrahman/assignment1_390/MainActivity.java
+++ app/src/main/java/ca/salmanrahman/assignment1_390/MainActivity.java
@@ -2,12 +2,21 @@ package ca.salmanrahman.assignment1_390;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gradeActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
+
 }
