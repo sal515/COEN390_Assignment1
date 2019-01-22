e15bb7b51185583ce0f273946ab78acf4374a39e
Salman <rahman_5@live.ca>
2019-01-22 03:33:34 -0500

    - Working on the layout of the GradeActivity and its layout. - Add custom adapter for the ListView - Generating custom rows for the ListView. To Do - Extract the AssignmentArrayList - Adding textView dynamically - Create ID resource xml file for textViews - Use for loop to make multiple textView

--- app/src/main/java/ca/salmanrahman/assignment1_390/CustomAdapterGradeActivity.java
+++ app/src/main/java/ca/salmanrahman/assignment1_390/CustomAdapterGradeActivity.java
@@ -1,6 +1,7 @@
 package ca.salmanrahman.assignment1_390;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,27 +39,39 @@ public class CustomAdapterGradeActivity extends ArrayAdapter<Course> {
         courseTitle.setText(course.getCourseTitle());
         courseAverage.setText("50");
 
-
+        TextView textView = new TextView(getContext());
+        RelativeLayout relativeLayout = convertView.findViewById(R.id.customRowsGA);
+        generateAssignmentTextView(relativeLayout, textView);
+        textView.setText(course.getCourseTitle());
 
         // return the completed view to render on screen
         return convertView;
     }
 
 
-    public void generateAssignmentTextView(RelativeLayout relativeLayout) {
+    public void generateAssignmentTextView(RelativeLayout relativeLayout, TextView textView ) {
 
-
-
-        TextView textView = new TextView(getContext());
-        textView.setText("This is Test");
+        int allPadding = 10;
 
         RelativeLayout.LayoutParams textViewLayout = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         textViewLayout.addRule(RelativeLayout.BELOW, R.id.courseTitleIDGA);
+        textView.setPadding(dpToPx(allPadding), dpToPx(allPadding), dpToPx(allPadding), dpToPx(allPadding));
         textView.setLayoutParams(textViewLayout);
-
-
-//        relativeLayout.addView();
+        relativeLayout.addView(textView);
     }
+
+    // https://bit.ly/2DsU7q3
+    public int pxToDp(int px)
+    {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+    // https://bit.ly/2DsU7q3
+    public int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+
 
 }
 
